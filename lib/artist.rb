af853bf6564ae78c61bc718fce2f7a9b3f0ced53
Nilton <niltonroese@gmail.com>
2022-07-05 19:40:38 +1000

    fix

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,8 @@
 class Artist
     attr_accessor :name
 
-    @@all = []
-
     def initialize (name)
         @name = name
-    end
-
-    def self.all
-        @@all
     end
 
     def songs
--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
     end
 
     def artist_name
-        artist.name if artist
-      end
+        self.artist.name if artist
+    end
 
 end
